3b5f6f0bad00a1d29c875981f2790f73d601d770
Harjitsingh001 <1429harjitsingh@gmail.com.com>
2024-05-17 15:31:15 +0530

    about

--- frontend/src/components/More/About.js
+++ frontend/src/components/More/About.js
@@ -1,24 +1,51 @@
 import React from 'react'
-import"./About.css"
+import "./About.css"
 const About = () => {
   return (
-    // <div>
-    //       <h1 class="centered">Centered</h1>
+    <>
+      <div className="container">
+        <img src="https://www.glofox.com/wp-content/uploads/2021/02/gym-website.jpg" alt="Snow" style={{ width: "100%", position: "sticky", height: "400px" }} />
 
-    //     <img src='https://www.glofox.com/wp-content/uploads/2021/02/gym-website.jpg'></img>
-    // </div>
+        <h1 className="centered">Build  Your {""}<span style={{ color: "orange", fontSize: "70px" }}>
+          Body
+        </span>
+          <span style={{ marginLeft: "10px" }} >{" "}
+            Transform Your <span style={{ color: "green", fontSize: "70px" }}>Life</span>
+          </span>
+        </h1>
+      </div>
 
-<div className="container">
-<img src="https://www.glofox.com/wp-content/uploads/2021/02/gym-website.jpg" alt="Snow" style={{width:"100%",position:"sticky"}}/>
+      <div className='mainDiv'>
 
-<h1 className="centered">Build  Your {""}<span style={{color:"red",fontSize:"50px"}}>
-Body 
-</span>
-<span >{" "}
-  Transform Your <span style={{color:"green",fontSize:"50px"}}>Life</span>
-</span>
- </h1>
-</div>
+        <div className='leftSide'>
+
+          <img src='https://static.vecteezy.com/system/resources/thumbnails/000/539/724/small/dumbbell_2__28b_w_29.jpg' style={{ height: "100px", width: "200px", mixBlendMode: "multiply" }}></img>
+          <h2 style={{ color: "orange", textDecoration: "none" }}>PORTTAMT NULLA QUAM NUNC, ACCUMSAN CONGUE
+          </h2>
+          <h1 style={{ color: "grey" }}>
+            Explore Our offer
+          </h1>
+
+        </div>
+
+        <div className='rightSide'>
+
+          <div className='upper'>
+            <h4>left1</h4>
+            <h4>left2</h4>
+          </div>
+
+          <div className='upper'>
+            <h4>right1</h4>
+            <h4>right2</h4>
+          </div>
+        </div>
+
+      </div>
+
+    </>
+
+
   )
 }
 
